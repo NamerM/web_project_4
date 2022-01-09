64a8906d85f69daa4a630a1b3b8b8b4f99d34904
Melih Namer <mnamer12@outlook.com>
2022-01-09 13:25:32 +0200

    index.js edit, popup & popup__open as well

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-//Profile Section Header Elements
-//const page = document.querySelector(".page");
+
 const header = document.querySelector(".header");
 const content = document.querySelector(".content");
 const footer = document.querySelector(".footer");
 const profile = document.querySelector(".profile");
 const editButton = document.querySelector (".profile__button");
-//Profile Section Content Elements
+
 const profileInfo = document.querySelector('.profile__info');
 const profileName = profileInfo.querySelector('.profile__header');
 const profileProfession = profileInfo.querySelector('.profile__profession');
-//Popup-Form Section Elements
+
 const profileForm = document.forms.profileForm;
 const profilePopup = document.querySelector('.popup');
 const saveButton = document.querySelector('.popup__save'); //popup.querySelector
@@ -17,12 +16,12 @@ let inputName = document.querySelector('.popup__input-name');
 let inputProfession = document.querySelector('.popup__input-profession');
 
 editButton.addEventListener('click', function(event) {
-  editButton.style.display = 'flex';
-  profilePopup.style.display = 'block';
-  //profilePopup.classList.toggle('popup_open');
-  header.setAttribute('style', 'opacity:0.5');
-  content.setAttribute('style', 'opacity:0.5');
-  footer.setAttribute('style', 'opacity:0.5');  // I tried page.setAttribute but popup is also effected. So tried also setting Attribute for popup but didn't work//
+  //editButton.style.display = 'flex';
+  //profilePopup.style.display = 'block';
+  profilePopup.classList.toggle('popup_open');
+  //header.setAttribute('style', 'opacity:0.5');
+  //content.setAttribute('style', 'opacity:0.5');
+ // footer.setAttribute('style', 'opacity:0.5');  // I tried page.setAttribute but popup is also effected. So tried also setting Attribute for popup but didn't work//
 });
 
 inputName.value = profileName.textContent;
@@ -31,12 +30,12 @@ inputProfession.value = profileProfession.textContent;
 profileForm.addEventListener('submit', function (event) {
   profileName.textContent =inputName.value;
   profileProfession.textContent = inputProfession.value;
-  profilePopup.style.display = 'none';
+  //profilePopup.style.display = 'none';
   event.preventDefault();
-  header.removeAttribute('style');
-  content.removeAttribute('style');
-  footer.removeAttribute('style');
-  //profilePopup.classList.remove('popup_open');
+ //header.removeAttribute('style');
+  //content.removeAttribute('style');
+  //footer.removeAttribute('style');
+  profilePopup.classList.remove('popup_open');
 });
 
 const addButton = document.querySelector(".add-button");
