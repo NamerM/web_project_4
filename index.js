c0d96fa59ab81857f512c0fab3d37cfc3044daf2
Melih Namer <mnamer12@outlook.com>
2022-01-11 00:41:03 +0200

    Javascript fixes

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-//const header = document.querySelector(".header");
-//const content = document.querySelector(".content");
-//const footer = document.querySelector(".footer");
 const profile = document.querySelector(".profile");
 const editButton = document.querySelector (".profile__button");
 
@@ -15,36 +12,24 @@ const closeButton = profilePopup.querySelector('.popup__close');
 let inputName = profilePopup.querySelector('.popup__input-name');
 let inputProfession = profilePopup.querySelector('.popup__input-profession');
 
-//editButton.addEventListener('click', function(event) {
-  //editButton.style.display = 'flex';
-  //profilePopup.style.display = 'block';
-  //header.setAttribute('style', 'opacity:0.5');
-  //content.setAttribute('style', 'opacity:0.5');
- // footer.setAttribute('style', 'opacity:0.5');  // I tried page.setAttribute but popup is also effected. So tried also setting Attribute for popup but didn't work, will be happy to learn how it will be if we did with js style attributes.that's why I left style.attribute lines - to learn - thx//
-//});
-
 function formLoadUp () {
+  profilePopup.classList.toggle('popup_open')
   inputName.value = profileName.textContent;
   inputProfession.value = profileProfession.textContent;
 }
 
-function togglePopup () {
-  profilePopup.classList.toggle('popup_open');
+function closePopup () {
+  profilePopup.classList.remove('popup_open');
 }
 
 function formProfileHandle(event) {
   profileName.textContent =inputName.value;
   profileProfession.textContent = inputProfession.value;
-  //profilePopup.style.display = 'none';
   event.preventDefault();
- //header.removeAttribute('style');
-  //content.removeAttribute('style');
-  //footer.removeAttribute('style');
   profilePopup.classList.remove('popup_open');
 }
 
-editButton.addEventListener('click', togglePopup);
 editButton.addEventListener('click', formLoadUp);
 profileForm.addEventListener('submit', formProfileHandle);
-closeButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', closePopup);
 
