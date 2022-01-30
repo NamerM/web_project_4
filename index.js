09ecefdd243dfeb22b112c546b830db7c23e22fe
Melih Namer <mnamer12@outlook.com>
2022-01-30 11:33:08 +0200

    cards part script addition

--- index.js
+++ index.js
@@ -1,9 +1,62 @@
+const initialCards = [
+{
+  name: "Yosemite Valley",
+  link: "https://namerm.github.io/web_project_4/images/card-image-yosemite.png",
+},
+{
+  name: "Lake Louise",
+  link: "https://namerm.github.io/web_project_4/images/card-image-lake-loise.png",
+},
+{
+  name: "Bald Mountains",
+  link: "https://namerm.github.io/web_project_4/images/card-image-bald-mountains.png",
+},
+{
+  name: "Latemar",
+  link: "https://namerm.github.io/web_project_4/images/card-image-latemar.png",
+},
+{
+  name: "Vanoise National Park",
+  link: "https://namerm.github.io/web_project_4/images/card-image-vanoise-nation.png",
+},
+{
+  name: "Lago di Braies",
+  link: "https://namerm.github.io/web_project_4/images/card-image-lago-di-braies.png",
+}
+];
+
+const cardTemplate = document
+.querySelector("#card-template")
+.content.querySelector(".elements__card");
+
+const cardElement = cardTemplate.cloneNode(true);
+const listContainer = document.querySelector('.elements__Cards');
+
+const createNewElementsCard = (title, imageSrc) => {
+  const clonedElementsCard = cardTemplate.cloneNode(true);
+  clonedElementsCard.style = 'background-image: url(" + imageSrc + ");';
+  const cardTitle = clonedElementsCard.querySelector(".elements__card-text");
+  cardTitle.textContent = title;
+  return clonedElementsCard;
+}
+
+const cardsStack = ()=>{
+  const listContainer = document.querySelector('.elements__cards');
+  const initialCardsImage = document.querySelector('.elements__image');
+    initialCards.forEach((item)=>{
+    listContainer.append(createNewElementsCard(item.name, item.link));
+ });
+};
+
+cardsStack();
+
 const profile = document.querySelector(".profile");
 const editButton = document.querySelector (".profile__button");
 
 const profileInfo = document.querySelector('.profile__info');
 const profileName = profileInfo.querySelector('.profile__header');
 const profileProfession = profileInfo.querySelector('.profile__profession');
+
 
 const profileForm = document.forms.profileForm;
 const profilePopup = document.querySelector('.popup');
@@ -13,7 +66,7 @@ let inputName = profilePopup.querySelector('.popup__input_type_name');
 let inputProfession = profilePopup.querySelector('.popup__input_type_profession');
 
 function formLoadUp () {
-  profilePopup.classList.toggle('popup_open')
+  profilePopup.classList.toggle('popup_open');
   inputName.value = profileName.textContent;
   inputProfession.value = profileProfession.textContent;
 }
@@ -23,7 +76,7 @@ function closePopup () {
 }
 
 function formProfileHandle(event) {
-  profileName.textContent =inputName.value;
+  profileName.textContent = inputName.value;
   profileProfession.textContent = inputProfession.value;
   event.preventDefault();
   profilePopup.classList.remove('popup_open');
