c59c0a6578979408fdfff439d4ce83264c7db16f
Melih Namer <mnamer12@outlook.com>
2022-02-17 01:24:19 +0200

    validation css checked + validation.js addons

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,20 +1,43 @@
 //// FORM VALIDATION PART ///
 
-
 //const formError = form.querySelector(`.${formInput.id}-error`);
 
-const checkInputValidity = (input) => {
+const showInputError = (input, formElement, {errorClass} ) => {
+  const errorElement = formElement.querySelector("#" + input.id + "-error");
+  // error msg and class
+  console.log(input.validationMessage);
+  input.classList.add(errorClass);
+  errorElement.textContent = input.validationMessage;
+  errorElement.classList.add(inputErrorClass);
+};
 
-}
+const hideInputError = (input, formElement, {errorClass} ) => {
+  const errorElement = formElement.querySelector("#" + input.id + "-error");
+  // error msg and class
+  errorElement.textcontent = "";
+  errorElement.classList.remove(errorClass);
+};
+
+const checkInputValidity = (formElement, input, settings) => {
+  console.log(input.validity.valid);
+  if(input.validity.valid) {
+    console.log('valid');
+    hideInputError();
+  } else {
+    showInputError(input, formElement, settings);
+  //console.log('validity', input.validity.valid);
+  //console.log('error message', input.validationMessage);
+  }
+};
 
 const setEventListeners = (formElement, settings) => {
   const inputs = Array.from(formElement.querySelectorAll(settings.inputSelector)); // [...formElement.querySelectorAll(settings.inputSelector)]
     inputs.forEach( (input) => {
       input.addEventListener('input', (evt) => {
-          console.log(evt);
+        console.log(evt);
           //check validity
-          checkInputValidity(input, settings);
-          // error msg and class
+        checkInputValidity(formElement, input, settings);
+
           // toggle submit button
     });
   })
