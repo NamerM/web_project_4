b677930cf246ac1778404cd5acfd956606be8fb1
Melih Namer <mnamer12@outlook.com>
2022-02-16 16:01:49 +0200

    popup__input_type_error.css typo corrected on naming

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,10 +1,25 @@
 //// FORM VALIDATION PART ///
 
-const form = document.querySelector(".popup");
-const formInput = form.querySelector(".pop-up__input");
-const formError = form.querySelector(`.${formInput.id}-error`);
+//const formElement = document.querySelector(".popup");
+//const formInput = form.querySelector(".pop-up__input");
+//const formError = form.querySelector(`.${formInput.id}-error`);
 
-//const showInputError =
+//const showInputError = (element) => {
+//element.classList.add("form__input_type_eror");
+//}
 
 
-//const hideInputError =
+//const hideInputError = (element) => {
+//element.classList.add("form__input_type_error");
+//};
+
+const enableValidation = (settings) => {};
+console.log(settings);
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible"
+});
