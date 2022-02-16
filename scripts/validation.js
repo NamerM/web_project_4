f4ec825065935287efd408728140bb9019ac8319
Melih Namer <mnamer12@outlook.com>
2022-02-16 15:54:09 +0200

    Adding error.css files in input & blank validation.js file

---
+++ scripts/validation.js
@@ -0,0 +1,10 @@
+//// FORM VALIDATION PART ///
+
+const form = document.querySelector(".popup");
+const formInput = form.querySelector(".pop-up__input");
+const formError = form.querySelector(`.${formInput.id}-error`);
+
+//const showInputError =
+
+
+//const hideInputError =
