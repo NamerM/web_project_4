735a55e286ec7b8404e46fa76023206bbbd8e8e3
Melih Namer <mnamer12@outlook.com>
2022-06-14 12:42:36 +0300

    webpack setup complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -27,7 +28,22 @@ module.exports = {
         test: /\.js$/,
         loader: "babel-loader",
         exclude: "/node_modules/"
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 }
+          },
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource"
+      },
     ]
   },
   plugins: [
@@ -35,5 +51,6 @@ module.exports = {
       template: "./src/index.html"
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
   ]
 }
